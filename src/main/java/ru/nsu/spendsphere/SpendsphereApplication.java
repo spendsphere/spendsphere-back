cc033b447edc1c262e40b7714cd89c088ba23ff7
an-mrv <nastya170204@yandex.ru>
2025-10-05 00:13:01 +0700

    initial commit + swagger config

---
+++ src/main/java/ru/nsu/spendsphere/SpendsphereApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.spendsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendsphereApplication.class, args);
+	}
+
+}
