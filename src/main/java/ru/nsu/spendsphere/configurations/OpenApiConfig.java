8a8477e9c5bc5a3b800162aef92d63b3cad4df00
d.barsukova <d.barsukova@g.nsu.ru>
2025-10-06 19:58:24 +0700

    fix&improve ci/cd

--- src/main/java/ru/nsu/spendsphere/configurations/OpenApiConfig.java
+++ src/main/java/ru/nsu/spendsphere/configurations/OpenApiConfig.java
@@ -1,6 +1,5 @@
 package ru.nsu.spendsphere.configurations;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +8,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("SpendSphere API Documentation")
-                        .version("1.0")
-                        .description("API documentation for SpendSphere application"));
-    }
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("SpendSphere API Documentation")
+                .version("1.0")
+                .description("API documentation for SpendSphere application"));
+  }
 }
