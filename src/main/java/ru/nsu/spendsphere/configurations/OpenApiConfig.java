d172681eafb6ac106fe5fdba24ae6800773cd282
d.barsukova <d.barsukova@g.nsu.ru>
2025-11-04 13:21:47 +0700

    imports

--- src/main/java/ru/nsu/spendsphere/configurations/OpenApiConfig.java
+++ src/main/java/ru/nsu/spendsphere/configurations/OpenApiConfig.java
@@ -3,7 +3,11 @@ package ru.nsu.spendsphere.configurations;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
