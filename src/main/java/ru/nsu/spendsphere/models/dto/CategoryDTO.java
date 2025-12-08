a842023ad5dd41ec51c9527db63a2eda45b2e405
an-mrv <nastya170204@yandex.ru>
2025-12-08 23:02:36 +0700

    Back and front connection

--- src/main/java/ru/nsu/spendsphere/models/dto/CategoryDTO.java
+++ src/main/java/ru/nsu/spendsphere/models/dto/CategoryDTO.java
@@ -1,6 +1,7 @@
 package ru.nsu.spendsphere.models.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import ru.nsu.spendsphere.models.entities.CategoryType;
 
 @Schema(description = "Информация о категории")
 public record CategoryDTO(
@@ -8,4 +9,5 @@ public record CategoryDTO(
     @Schema(description = "Название категории", example = "Продукты") String name,
     @Schema(description = "URL иконки категории", example = "https://example.com/food-icon.png")
         String iconUrl,
-    @Schema(description = "Флаг дефолтной категории", example = "false") Boolean isDefault) {}
+    @Schema(description = "Флаг дефолтной категории", example = "false") Boolean isDefault,
+    @Schema(description = "Тип категории", example = "EXPENSE") CategoryType categoryType) {}
